69405893dacdd6c884d0452bc22504c714fcacf0
Hasan Amirov <a.amirovv@mail.ru>
2021-01-02 02:46:54 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Create',
+    component: () => import('../views/Create.vue')
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: () => import('../views/List.vue')
+  },
+  {
+    path: '/task/:id',
+    name: 'Task',
+    component: () => import('../views/Task.vue')
+  }
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
